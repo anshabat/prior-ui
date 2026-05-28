7ab613b20f63f5c26b87a2ee841e3bf2e49f1226
anshabat <andriyshabat@gmail.com>
2026-05-28 20:22:03 +0300

    infra: config knip

--- apps/auth/eslint.config.js
+++ apps/auth/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig([
   globalIgnores(["dist"]),
